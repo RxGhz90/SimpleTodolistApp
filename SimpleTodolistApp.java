c2507345b013f3067c544ed1bbd8beba315fd677
Firas Ghazi <firasghz90@gmail.com>
2025-02-23 20:41:54 +0700

    add model, business logic, and view

---
+++ SimpleTodolistApp.java
@@ -0,0 +1,32 @@
+public class SimpleTodolistApp {
+    private static String[] todolist = new String[10];
+
+    public static void main(String[] args) {
+        
+    }
+
+    /**
+     * Show todolist
+     */
+    public static void showTodolist() {}
+
+    /**
+     * Add todolist
+     */
+    public static void addTodolist() {}
+
+    /**
+     * Remove todolist
+     */
+    public static void removeTodolist() {}
+
+
+    /**
+     * For view each fucntion todolist
+     */
+    public static void viewShowTodolist() {}
+
+    public static void viewAddTodolist() {}
+
+    public static void viewRemoveTodolist() {}
+}
